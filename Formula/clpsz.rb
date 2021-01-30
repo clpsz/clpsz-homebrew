ce5b8a4673cdb62a65564cca0295237764b8c573
clpsz <pengclpsz@gmail.com>
2021-01-30 17:04:13 +0800

    update download url

--- Formula/clpsz.rb
+++ Formula/clpsz.rb
@@ -3,7 +3,7 @@ require "formula"
 class Clpsz < Formula
   desc "clpsz personal tools"
   homepage "https://github.com/clpsz/homebrew-clpsz"
-  url "https://github.com/clpsz/homebrew-clpsz/releases/download/v0.0.1/clpsz-darwin-amd64-0.0.1.tar.gz"
+  url "https://github.com/clpsz/homebrew-clpsz/releases/download/v0.0.1/clpsz-0.0.1.tar.gz"
   sha256 "42b3951d85da12317c1c1a1d9c5bb91ac19ae82e9355127e31fe83368e7b2bfe"
   head "https://github.com/clpsz/homebrew-clpsz.git"
 
